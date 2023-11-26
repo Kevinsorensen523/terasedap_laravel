ee0e65441f16609ab6b180d09689d0f8021175d3
Kevinsorensen523 <94046712+Kevinsorensen523@users.noreply.github.com>
2023-11-26 18:27:45 +0700

    Login Middleware

--- resources/views/add_food_item.blade.php
+++ resources/views/add_food_item.blade.php
@@ -17,7 +17,21 @@
         <ul>
             <li><a href="#">Menu</a></li>
             <li><a href="{{ route('orders.history') }}">Orders</a></li>
-            <li><a href="#">Logout</a></li>
+            <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"
+                            >Logout</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
+            </li>
         </ul>
     </div>
 
